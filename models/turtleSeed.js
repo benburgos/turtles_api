f453fd7b6c0fc2994092a0857b7811f04d920b02
Ben <ben.a.burgos@gmail.com>
2022-05-17 20:46:06 -0700

    completes lab

---
+++ models/turtleSeed.js
@@ -0,0 +1,42 @@
+module.exports = [
+    {
+        "name": "Leonardo",
+        "role": "ninja"
+      },
+      {
+        "name": "Michaelangelo",
+        "role": "ninja"
+      },
+      {
+        "name": "Donatello",
+        "role": "ninja"
+      },
+      {
+        "name": "Raphael",
+        "role": "ninja"
+      },
+      {
+        "name": "Franklin",
+        "role": "student"
+      },
+      {
+        "name": "Master Oogway",
+        "role": "sensei"
+      },
+      {
+        "name": "Crush",
+        "role": "surfer"
+      },
+      {
+        "name": "Koopa",
+        "role": "enemy"
+      },
+      {
+        "name": "Bowser",
+        "role": "king of evil mario turtles"
+      },
+      {
+        "name": "Squirtle",
+        "role": "pokemon"
+      }
+]
---
+++ server.js
@@ -0,0 +1,89 @@
+// Dependencies
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const turtleSeed = require('./models/turtleSeed');
+
+// Create the application object
+const app = express();
+// Destructure mongoose for Schema/model
+const { Schema, model } = mongoose;
+
+// Create the DB connection
+const DATABASE_URL = process.env.DATABASE_URL;
+const config = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(DATABASE_URL, config);
+mongoose.connection
+  .on('open', () => console.log('Connected to database!'))
+  .on('close', () => console.log('Disconnected from database!'))
+  .on('error', (err) => console.log(err));
+
+// Create the data schema
+const turtleSchema = new Schema({
+  name: String,
+  role: String,
+});
+const Turtle = model('Turtle', turtleSchema);
+
+// Middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Home Route
+app.get('/', (req, res) => {
+  res.json({ response: 'I like turtles!' });
+});
+
+// INDUCES
+// Index Route
+app.get('/turtles', async (req, res) => {
+  const turtles = await Turtle.find({});
+  res.json(turtles);
+});
+
+// Seed Route
+app.get('/turtles/seed', async (req, res) => {
+  await Turtle.deleteMany({}).catch((err) => res.send(err));
+  const turtles = await Turtle.create(turtleSeed).catch((err) => res.send(err));
+  res.json(turtles);
+});
+
+// Delete Route
+app.delete('/turtles/:id', async (req, res) => {
+  await Turtle.findByIdAndDelete(req.params.id).catch((err) => res.send(err));
+  res.redirect('/turtles');
+});
+
+// Update Route
+app.put('/turtles/:id', async (req, res) => {
+  const turtle = await Turtle.findByIdAndUpdate(req.params.id, req.body).catch(
+    (err) => res.send(err)
+  );
+  await turtle.save();
+  res.redirect('/turtles');
+});
+
+// Create Route
+app.post('/turtles', async (req, res) => {
+  await Turtle.create(req.body).catch((err) => res.send(err));
+  res.redirect('/turtles');
+});
+
+// Show Route
+app.get('/turtles/:id', async (req, res) => {
+  const turtle = await Turtle.findById(req.params.id).catch((err) =>
+    res.send(err)
+  );
+  res.json(turtle);
+});
+
+// Listener
+const PORT = process.env.PORT || 1337;
+app.listen(PORT, () => {
+  console.log(`You're connected on port ${PORT}!`);
+});
